fc9564c1c4a286028a3c0f85f326face0676baa7
Sara_Abbasghorbani <saraabbasghorbani04@gmail.com>
2026-08-08 13:15:52 +0330

    Configure Spring Security

---
+++ src/main/java/com/summer_project/demo/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.summer_project.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable()).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authorizeHttpRequests(auth -> auth.requestMatchers("/users/register", "/users/login").permitAll().anyRequest().authenticated());
+        return http.build();
+    }
+}
