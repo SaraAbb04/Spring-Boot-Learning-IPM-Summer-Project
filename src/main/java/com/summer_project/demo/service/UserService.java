951225050e5f3a179d5336ae85134c75c2d60e9c
Sara_Abbasghorbani <saraabbasghorbani04@gmail.com>
2026-08-08 11:40:41 +0330

    Add password hashing with BCrypt

--- src/main/java/com/summer_project/demo/service/UserService.java
+++ src/main/java/com/summer_project/demo/service/UserService.java
@@ -2,20 +2,25 @@ package com.summer_project.demo.service;
 
 import com.summer_project.demo.model.User;
 import com.summer_project.demo.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    public UserService(UserRepository userRepository){
+    private final PasswordEncoder passwordEncoder;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
     public User register(User user){
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         return (userRepository.save(user));
     }
     public User Login(String email, String password){
         User user = userRepository.findByEmail(email).orElseThrow(()-> new RuntimeException("Invalid password"));
-        if(!user.getPassword().equals(password)){
+        if(!passwordEncoder.matches(password, user.getPassword())){
             throw new RuntimeException("Invalid password");
         }
         return user;
