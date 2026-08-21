bbd3dd7bac8349c88d46a59fe319cb5507ec26a5
Sara_Abbasghorbani <saraabbasghorbani04@gmail.com>
2026-08-21 16:34:03 +0330

    update UserService for handle exceptions

--- src/main/java/com/summer_project/demo/service/UserService.java
+++ src/main/java/com/summer_project/demo/service/UserService.java
@@ -2,6 +2,9 @@ package com.summer_project.demo.service;
 
 import com.summer_project.demo.dto.ChangePasswordRequest;
 import com.summer_project.demo.dto.UpdateProfileRequest;
+import com.summer_project.demo.exception.EmailAlreadyExistsException;
+import com.summer_project.demo.exception.InvalidPasswordException;
+import com.summer_project.demo.exception.UserNotFoundException;
 import com.summer_project.demo.model.User;
 import com.summer_project.demo.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -15,32 +18,35 @@ public class UserService {
 
 
     public User register(User user){
+        if(userRepository.findByEmail(user.getEmail()).isPresent()){
+            throw new EmailAlreadyExistsException("Email already exits!");
+        }
         user.setRole("USER");
         return (userRepository.save(user));
     }
 
 
     public User Login(String email, String password){
-        User user = userRepository.findByEmail(email).orElseThrow(()-> new RuntimeException("Invalid email or password" ));
+        User user = userRepository.findByEmail(email).orElseThrow(()-> new UserNotFoundException("User with this email or password not found!" ));
         if(!password.equals(user.getPassword())){
-            throw new RuntimeException("Password is incorrect");
+            throw new InvalidPasswordException("Password is incorrect");
         }
         return user;
     }
     public User getProfile(String email){
-        return userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User Not Found!"));
+        return userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("User Not Found!"));
     }
 
     public User updateProfile(String currentEmail, UpdateProfileRequest request) {
-        User user = userRepository.findByEmail(currentEmail).orElseThrow(() -> new RuntimeException("User Not Found!"));
+        User user = userRepository.findByEmail(currentEmail).orElseThrow(() -> new UserNotFoundException("User Not Found!"));
         user.setUsername(request.getUserName());
         user.setEmail(request.getEmail());
         return userRepository.save(user);
     }
     public void changePassword(String email, ChangePasswordRequest request){
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found!"));
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("User not found!"));
         if(!request.getCurrentPassword().equals(user.getPassword())){
-            throw new RuntimeException("Current password is incorrect");
+            throw new InvalidPasswordException("Current password is incorrect");
         }
         user.setPassword(request.getNewPassword());
         userRepository.save(user);
